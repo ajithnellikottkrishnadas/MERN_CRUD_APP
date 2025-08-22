df29f237b1d789387f90cc5685986c1906c920cd
ajithnellikottkrishnadas <ajithnellikodanofficial@gmail.com>
2025-08-22 12:18:09 +0530

    Changed design of frontend , added add edit delete button in dahsboard

---
+++ client/src/AddCustomer/AddCustomer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddCustomer = () => {
+  return (
+    <div>
+       <h1>heyy</h1>
+    </div>
+  )
+}
+
+export default AddCustomer
--- client/src/getuser/getCustomer.jsx
+++ client/src/getuser/getCustomer.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./User.css"
+import AddIcon from "@mui/icons-material/Add";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 const GetCustomers = () => {
   const [customers, setCustomers] = useState([]);
   const [error, setError] = useState("");
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchCustomers = async () => {
@@ -32,32 +39,74 @@ const GetCustomers = () => {
   }, []);
 
   return (
-    <div class="container">
-      <table class="responsive-table">
-        <caption>DASHBOARD</caption>
+    <div className="container">
+
+      <div className="buttonStyle" style={{ marginBottom: "1em", textAlign: "right" }}>
+        <h3>DASHBOARD</h3>
+
+        <Button
+          variant="contained"
+          to="/addCustomer"
+          color="primary"
+          startIcon={<AddIcon />}
+          onClick={() => navigate("/addCustomer")}
+        >
+          Add Customer
+        </Button>
+      </div>
+      <table className="responsive-table">
+
         <thead>
+
           <tr>
             <th scope="col">No</th>
             <th scope="col">Name</th>
             <th scope="col">Email</th>
             <th scope="col">Address</th>
             <th scope="col">Phone</th>
+
           </tr>
         </thead>
         <tfoot>
 
         </tfoot>
-        <tbody>{customers.map((customer, index) => {
-          <tr>
-            <th scope="row">{customer.index}</th>
-            <td data-title="Released">{customer.name}</td>
-            <td data-title="Studio">{customer.email}</td>
-            <td data-title="Worldwide Gross" >{customer.address}</td>
-            <td data-title="Domestic Gross" >{customer.phone}</td>
-          </tr>
-        })}
+        <tbody>
+          {customers.length > 0 ? (
+            customers.map((customer, index) => (
+              <tr key={customer._id || index}>
+                <th scope="row">{index + 1}</th>
+                <td data-title="Name">{customer.name}</td>
+                <td data-title="Email">{customer.email}</td>
+                <td data-title="Address">{customer.address}</td>
+                <td data-title="Phone">
+                  <div className="iconDes">
+                    <div>{customer.phone} </div>
+                    <div>
+                      <button
+                        className="boton-elegante"
+                        // onClick={() => handleEdit(customer._id)}
+                      >
+                        <EditIcon />
+                      </button>
 
+                      <button
+                        className="boton-elegante second"
+                        // onClick={() => handleDelete(customer._id)}
+                      >
+                        <DeleteIcon /></button>
+                    </div>
+                  </div>
+                </td>
 
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="5" style={{ textAlign: "center" }}>
+                No customers found
+              </td>
+            </tr>
+          )}
         </tbody>
       </table>
     </div>
